ef0156ae044dc87e115b02dbade111f0d1c5b7e0
chrisezedi <91887106+chrisezedi@users.noreply.github.com>
2023-02-10 06:59:25 +0100

    add authentication

--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,31 @@
-from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework import (
+    generics,
+    authentication,
+    permissions
+)
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer
+)
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
+
+
+class CreateAuthTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
