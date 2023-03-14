01a0e8a86acc9efa8bb275cb3ca3a4fc8ab97b3a
chrisezedi <91887106+chrisezedi@users.noreply.github.com>
2023-03-14 09:22:01 +0100

    add list all user and auth user recipes tests

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -11,34 +11,53 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-# from core.models import (
-#     Recipe,
-# )
+from core.models import (
+    Recipe,
+)
 
-# from recipe.serializers import (
-#     RecipeSerializer
-# )
+from recipe.serializers import (
+    RecipeSerializer
+)
 
 RECIPE_URL = reverse('recipe:recipe-list')
 
 
+def create_recipe(user, **params):
+    defaults = {
+        'name': 'recipe1',
+        'description': 'recipe1 desc',
+        'time_minutes': 30,
+        'price': Decimal(300),
+        'link': 'recipe1.com'
+    }
+
+    defaults.update(params)
+
+    return Recipe.objects.create(user=user, **defaults)
+
+
+def create_user(**params):
+    defaults = {
+        'email': 'user@example.com',
+        'password': 'testpassword'
+    }
+
+    defaults.update(params)
+    return get_user_model().objects.create(**params)
+
+
 class privateRecipeApiTests(TestCase):
-    """create recipe test"""
-    """
-    Possible test cases
-     - create recipe success
-     - test invalid data
-     - test price is decimal
-    """
+    """Private Recipe API Tests"""
 
     def setUp(self):
-        user = get_user_model().objects.create(
-            email='user@example.com', password='testpassword')
+        """before each setup"""
+        user = create_user()
         self.client = APIClient()
         self.user = user
         self.client.force_authenticate(self.user)
 
     def test_create_recipe(self):
+        """create recipe"""
         payload = {
             'name': 'recipe1',
             'description': 'recipe1 desc',
@@ -50,3 +69,26 @@ class privateRecipeApiTests(TestCase):
         res = self.client.post(RECIPE_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         self.assertEqual(res.data['name'], payload['name'])
+
+    def test_list_all_recipes(self):
+        """retrieve all recipes"""
+        create_recipe(user=self.user)
+        create_recipe(user=self.user)
+
+        res = self.client.get(RECIPE_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 2)
+
+    def test_list_all_auth_user_recipes(self):
+        other_user = create_user(
+            email='otheruser@example.com', password='inspiredOTUS')
+        create_recipe(user=other_user)
+        create_recipe(user=self.user)
+
+        res = self.client.get(RECIPE_URL)
+        recipes = Recipe.objects.filter(user=self.user)
+        serializer = RecipeSerializer(recipes, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(len(serializer.data), 1)
+        self.assertEqual(res.data, serializer.data)
